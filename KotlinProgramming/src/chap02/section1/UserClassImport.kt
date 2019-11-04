d4aa91142c1b32aeedbd3c15b19bd902cc401a6e
seokjun <tjrwns8024@naver.com>
2019-11-04 23:02:46 +0900

    use[As]

--- KotlinProgramming/src/chap02/section1/UserClassImport.kt
+++ KotlinProgramming/src/chap02/section1/UserClassImport.kt
@@ -1,10 +1,16 @@
 package chap02.section1
 
-import com.example.edu.Person
+import com.example.edu.Person as User
 
 fun main(){
-    val user1 = Person("kildong", 30)
+    val user1 = User("kildong", 30)
+    val user2 = Person("asd", "sddk")
 
     println(user1.name)
     println(user1.age)
+
+    println(user2.id)
+    println(user2.name)
 }
+
+class Person(val id:String, val name:String)
